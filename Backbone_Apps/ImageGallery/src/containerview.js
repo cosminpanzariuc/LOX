c4e34624dc801937a0bf4858586548a3b667a55c
Cosmin Panzariuc <cosmin.panzariuc@gmail.com>
2016-01-17 23:05:53 +0200

    Using global variables

--- Backbone_Apps/ImageGallery/src/containerview.js
+++ Backbone_Apps/ImageGallery/src/containerview.js
@@ -1,19 +1,21 @@
-var ContainerView = Backbone.View.extend({
-    el: "#mainContainer",
-    initialize: function(attrs){
-        this.bus = attrs.bus;
-        this.bus.on("targetSelected", this.onTargetSelected, this);
-    },
-    
-    onTargetSelected: function(elem){
-        this.model = elem;
-        this.render();
-    },
-    
-    render: function(){
-        if(this.model){
-            this.$el.attr("src", this.model.get("picx"));
-        }  
-        return this;
-    }
-});
+(function(){
+    return ContainerView = Backbone.View.extend({
+        el: "#mainContainer",
+        initialize: function(attrs){
+            this.bus = attrs.bus;
+            this.bus.on("targetSelected", this.onTargetSelected, this);
+        },
+
+        onTargetSelected: function(elem){
+            this.model = elem;
+            this.render();
+        },
+
+        render: function(){
+            if(this.model){
+                this.$el.attr("src", this.model.get("picx"));
+            }  
+            return this;
+        }
+    });
+}());
