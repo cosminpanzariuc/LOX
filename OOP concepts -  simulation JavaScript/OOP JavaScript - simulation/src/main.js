af0abd69de5ba49bfd53dec69b1fb8c06f5eae50
Cosmin Panzariuc <cosmin.panzariuc@gmail.com>
2016-11-02 20:49:46 +0200

    refactoring code

--- OOP concepts -  simulation JavaScript/OOP JavaScript - simulation/src/main.js
+++ OOP concepts -  simulation JavaScript/OOP JavaScript - simulation/src/main.js
@@ -409,6 +409,51 @@
 
 //----------------------------------------------------------------
 
+////Mental Models Compared - OO Style
+//function Foo(who){
+//    this.me = who;
+//}
+//Foo.prototype.identify = function(){
+//    return "I am " + this.me;
+//}
+//
+//function Bar(who){
+//    Foo.call(this, who);
+//}
+//Bar.prototype = Object.create(Foo.prototype);
+//Bar.prototype.speak = function(){
+//    console.log("Hello, " + this.identify() + ".");
+//}
+//
+//var b1 = new Bar("b1");
+//var b2 = new Bar("b2");
+//b1.speak();
+//b2.speak();
+//
+//
+////Mental Models Compared - OLOO Style
+//var Foo = {
+//    init: function(who){
+//        this.me = who;
+//    },
+//    identify: function(){
+//        return "I am " + this.me;
+//    }
+//}
+//
+//var Bar = Object.create(Foo);
+//Bar.speak = function(){
+//    console.log("Hello, " + this.identify() + ".");
+//}
+//
+//var b1 = Object.create(Bar);
+//var b2 = Object.create(Bar);
+//b1.init("b1");
+//b2.init("b2");
+//b1.speak();
+//b2.speak();
+
+//----------------------------------------------------------------
 ////Mixins jQuery
 //function Person(name) {
 //    this.name = name;
