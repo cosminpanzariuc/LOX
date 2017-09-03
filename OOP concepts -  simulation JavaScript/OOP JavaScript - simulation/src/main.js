91f0387da41774f07f6a3c1c0b96a41a65b38fd6
Cosmin Panzariuc <cosmin.panzariuc@gmail.com>
2017-09-03 11:58:34 +0300

    add recursion

--- OOP concepts -  simulation JavaScript/OOP JavaScript - simulation/src/main.js
+++ OOP concepts -  simulation JavaScript/OOP JavaScript - simulation/src/main.js
@@ -539,15 +539,15 @@
 
 //----------------------------------------------------------------
 
-//Singleton Pattern
-//var Singleton = (function () {
+// //Singleton Pattern
+// var Singleton = (function () {
 //    var instance;
-// 
+//
 //    function createInstance() {
 //        var object = new Object("I am the instance");
 //        return object;
 //    }
-// 
+//
 //    return {
 //        getInstance: function () {
 //            if (!instance) {
@@ -556,16 +556,14 @@
 //            return instance;
 //        }
 //    };
-//})();
-// 
-//function run() {
-// 
+// }());
+//
+// function run() {
 //    var instance1 = Singleton.getInstance();
 //    var instance2 = Singleton.getInstance();
-// 
-//    alert("Same instance? " + (instance1 === instance2));  
-//}
-//run();
+//     alert("Same instance? " + (instance1 === instance2));
+// }
+// run();
 
 //----------------------------------------------------------------
 
