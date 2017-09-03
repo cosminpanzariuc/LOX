0cd5a0be3ee38b4995952188693df0f3a9de7e76
Cosmin Panzariuc <cosmin.panzariuc@gmail.com>
2017-09-03 10:23:30 +0300

    add recursion

--- OOP concepts -  simulation JavaScript/OOP JavaScript - simulation/src/main.js
+++ OOP concepts -  simulation JavaScript/OOP JavaScript - simulation/src/main.js
@@ -821,3 +821,33 @@
 // var breathLightningDragon = tinyDragon("lightning");
 // console.log(breathLightningDragon + " - Curry");
 
+
+// //Recursion
+// var categories = [
+//     {id: "animals", parent: null},
+//     {id: "mammals", parent: "animals"},
+//     {id: "cats", parent: "mammals"},
+//     {id: "dogs", parent: "mammals"},
+//     {id: "chihuahua", parent: "dogs"},
+//     {id: "labrador", parent: "dogs"},
+//     {id: "persian", parent: "cats"},
+//     {id: "siamese", parent: "cats"}
+// ];
+//
+// var makeTree = function(categories, parent){
+//     var node = {};
+//     categories
+//         .filter(function(c){
+//             return c.parent === parent
+//         })
+//         .forEach(function(c){
+//             return node[c.id] = makeTree(categories, c.id)
+//         });
+//     return node;
+// };
+//
+// console.log(JSON.stringify(
+//     makeTree(categories, null), null, 2
+// ));
+
+
