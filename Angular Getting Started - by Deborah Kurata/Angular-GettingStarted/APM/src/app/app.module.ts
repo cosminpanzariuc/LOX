7de9b6ee20e87cf98479e69725ca3f350119c374
Cosmin Panzariuc <cosmin.panzariuc@gmail.com>
2017-09-09 20:42:47 +0300

    add angular stuff

--- Angular Getting Started - by Deborah Kurata/Angular-GettingStarted/APM/src/app/app.module.ts
+++ Angular Getting Started - by Deborah Kurata/Angular-GettingStarted/APM/src/app/app.module.ts
@@ -1,11 +1,13 @@
+import { ConverToSpacesPipe } from './shared/convert-to-spaces.pipe';
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { AppComponent } from './app.component';
 import { ProductListComponent } from './products/product-list.component';
+import { StarComponent } from './shared/star.component';
 
 @NgModule({
-  declarations: [AppComponent, ProductListComponent],
+  declarations: [AppComponent, ProductListComponent, ConverToSpacesPipe, StarComponent],
   imports: [BrowserModule, FormsModule],
   providers: [],
   bootstrap: [AppComponent]
--- Angular Getting Started - by Deborah Kurata/Angular-GettingStarted/APM/src/app/products/product-list.component.ts
+++ Angular Getting Started - by Deborah Kurata/Angular-GettingStarted/APM/src/app/products/product-list.component.ts
@@ -1,19 +1,54 @@
-import { Component } from "@angular/core";
+import { Component, OnInit } from "@angular/core";
+import { IProduct } from "./product";
 
 @Component({
   selector: "pm-products",
-  templateUrl: "./product-list.component.html"
+  templateUrl: "./product-list.component.html",
+  styleUrls: ["./product-list.component.css"]
 })
-export class ProductListComponent {
+export class ProductListComponent implements OnInit {
   pageTitle: string = "Product List";
   imageWidth: number = 60;
   imageMargin: number = 2;
-  showImage: boolean = true;
-  listFilter: string = '';
-  toggleImage(): void{
+  showImage: boolean = false;
+  toggleImage(): void {
     this.showImage = !this.showImage;
   }
-  products: any[] = [
+  ngOnInit(): void {
+    console.log("In OnInit");
+  }
+
+  filteredProducts: IProduct[];
+  _listFilter: string;
+
+  constructor() {
+    this.filteredProducts = this.products;
+    this.listFilter = "";
+  }
+
+  get listFilter(): string {
+    return this._listFilter;
+  }
+  set listFilter(value: string) {
+    this._listFilter = value;
+    this.filteredProducts = this.listFilter
+      ? this.performFilter(this.listFilter)
+      : this.products;
+  }
+
+  performFilter(filterBy: string): IProduct[] {
+    filterBy = filterBy.toLocaleLowerCase();
+    return this.products.filter(
+      (product: IProduct) =>
+        product.productName.toLowerCase().indexOf(filterBy) !== -1
+    );
+  }
+
+  onRatingClicked(message: string): void {
+      this.pageTitle = 'Product List: ' + message;
+  }
+
+  products: IProduct[] = [
     {
       productId: 1,
       productName: "Leaf Rake",
