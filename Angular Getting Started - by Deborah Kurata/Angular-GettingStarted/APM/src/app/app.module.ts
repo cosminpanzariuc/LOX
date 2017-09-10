bebcbb9dda67171abfe3540f5351a3fa5c2915b8
Cosmin Panzariuc <cosmin.panzariuc@gmail.com>
2017-09-10 22:48:11 +0300

    add angular stuff

--- Angular Getting Started - by Deborah Kurata/Angular-GettingStarted/APM/src/app/app.module.ts
+++ Angular Getting Started - by Deborah Kurata/Angular-GettingStarted/APM/src/app/app.module.ts
@@ -1,3 +1,5 @@
+import { RouterModule } from '@angular/router';
+import { ProductDetailComponent } from './products/product-detail.component';
 import { HttpClientModule } from '@angular/common/http';
 import { ConverToSpacesPipe } from './shared/convert-to-spaces.pipe';
 import { BrowserModule } from '@angular/platform-browser';
@@ -6,11 +8,36 @@ import { FormsModule } from '@angular/forms';
 import { AppComponent } from './app.component';
 import { ProductListComponent } from './products/product-list.component';
 import { StarComponent } from './shared/star.component';
+import { WelcomeComponent } from './home/welcome.component';
+import { ProductGuardService } from './products/product-guard.service';
 
 @NgModule({
-  declarations: [AppComponent, ProductListComponent, ConverToSpacesPipe, StarComponent],
-  imports: [BrowserModule, FormsModule, HttpClientModule],
-  providers: [],
+  declarations: [
+    AppComponent,
+    ProductListComponent,
+    ConverToSpacesPipe,
+    StarComponent,
+    ProductDetailComponent,
+    WelcomeComponent
+  ],
+
+  imports: [
+    BrowserModule,
+    FormsModule,
+    HttpClientModule,
+    RouterModule.forRoot([
+      { path: 'products', component: ProductListComponent },
+      { path: 'products/:id', 
+        canActivate:[ ProductGuardService ],
+        component: ProductDetailComponent },
+      { path: 'welcome', component: WelcomeComponent },
+      { path: '', redirectTo: 'welcome', pathMatch: 'full' },
+      { path: '**', redirectTo: 'welcome', pathMatch: 'full' },
+    ])
+  ],
+
+  providers: [ProductGuardService],
+
   bootstrap: [AppComponent]
 })
 export class AppModule { }
