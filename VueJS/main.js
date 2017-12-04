93f25254b1537513c76d54c963d353c8f695d885
Cosmin Panzariuc <cosmin.panzariuc@gmail.com>
2017-12-04 22:34:55 +0200

    vue js stuff

--- VueJS/main.js
+++ VueJS/main.js
@@ -4,23 +4,29 @@ new Vue({
     el: '#app',
     data: {
         title: 'Hello World!',
+        name: 'Jesse',
         link: 'http://www.google.com',
-        finishedLink: '<a href="http://google.com">Google</a>',
+        finishedLink: '<a href="http://youtube.com">Youtube</a>',
         counter: 0
     },
     methods: {
         changeTitle: function (event) {
             this.title = event.target.value;
         },
-        renderTitle: function () {
-            return this.title;
-        },
+
         increase: function (step, event) {
             this.counter += step;
             console.log(event);
         },
-        alertMe: function(){
+        alertMe: function () {
             alert("ENTER!!!!!");
+        }
+    },
+
+    computed: {
+        renderTitle: function () {
+            console.log("intraaa");
+            return this.title;
         }
     }
 });
