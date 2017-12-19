0e517b8217970350314ad672d099bcc6abce2818
Cosmin Panzariuc <cosmin.panzariuc@gmail.com>
2017-12-19 22:07:56 +0200

    vue stuff

--- VueJS/main.js
+++ VueJS/main.js
@@ -4,38 +4,59 @@ new Vue({
     el: '#app',
     data: {
         title: 'Hello World!',
-        name: 'Jesse',
+        name: 'Double Binding',
         link: 'http://www.google.com',
         finishedLink: '<a href="http://youtube.com">Youtube</a>',
-        counter: 0
+        counter: 0,
+        isRed: false,
+        color: 'green',
+        progressBar: {
+            width: '0px',
+            backgroundColor: 'red'
+        }
     },
+
     methods: {
         changeTitle: function (event) {
-            // console.log(event);
             this.title = event.target.value;
         },
         increase: function (step, event) {
             this.counter += step;
-            // console.log(event);
         },
         alertMe: function () {
             alert("ENTER!!!!!");
+        },
+        startProgress: function(){
+            var vm = this;
+            var width = 0;
+            setInterval(function(){
+                width += 10;
+                vm.progressBar.width = width + "px";
+            }, 500);
         }
     },
 
     computed: {
         renderTitle: function () {
-            console.log("intraaa..");
             return this.title;
-        }
+        },
+        result: function(){
+            return this.counter == 10 ? "BINGO!!!" : "Not there yet..."
+        },
+        changeBetweenBlueAndRed: function(){
+            return{
+                red: this.isRed,
+                blue: !this.isRed
+            };
+        },
     },
 
     watch: {
-        counter: function(value){
+        result: function(value){
             var vm = this;
             setTimeout(function(){
                 vm.counter = 0;
-            }, 5000);
+            }, 2000);
         }
     }
 });
