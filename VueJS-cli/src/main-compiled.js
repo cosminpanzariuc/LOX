2ec99e8e2572e85ce6fb76800b93d5f0301f4000
Cosmin Panzariuc <cosmin.panzariuc@gmail.com>
2017-12-29 20:48:11 +0200

    add Vue CLI project

---
+++ VueJS-cli/src/main-compiled.js
@@ -0,0 +1,11 @@
+import Vue from 'vue';
+import App from './App.vue';
+
+new Vue({
+  el: '#app',
+  render: function render(h) {
+    return h(App);
+  }
+});
+
+//# sourceMappingURL=main-compiled.js.map
