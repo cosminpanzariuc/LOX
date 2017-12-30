d3c1c82b79de70ce764c0ffbe18752dcd5f8b2b5
Cosmin Panzariuc <cosmin.panzariuc@gmail.com>
2017-12-31 00:17:04 +0200

    add vue components

--- VueJS-cli/src/App.vue
+++ VueJS-cli/src/App.vue
@@ -1,13 +1,38 @@
 <template>
-  <h1>Hello World!</h1>
+  <div>
+    <app-header :title="title"></app-header>
+    <app-ninjas :ninjas="ninjasArr"></app-ninjas>
+    <app-footer :title="title"></app-footer>
+  </div>
 </template>
 
 <script>
-export default {
+  import Header from './components/Header.vue';
+  import Footer from './components/Footer.vue';
+  import Ninjas from './components/Ninjas.vue';
 
-}
+  export default {
+    components: {
+      'app-header': Header,
+      'app-footer': Footer,
+      'app-ninjas': Ninjas
+    },
+    data(){
+      return{
+        ninjasArr: [
+          {name: 'Ryu', speciality: 'Vue Components', show: false},
+          {name: 'Crystal', speciality: 'HTML Wizardry', show: false},
+          {name: 'Hitoshi', speciality: 'Click Events', show: false},
+          {name: 'Tango', speciality: 'Conditionals', show: false},
+          {name: 'Kami', speciality: 'Webpack', show: false},
+          {name: 'Yoshi', speciality: 'Data Diggin', show: false}
+        ],
+        title: 'Vue Ninjas'
+      }
+    }
+  }
 </script>
 
-<style lang="scss">
+<style>
 
 </style>
