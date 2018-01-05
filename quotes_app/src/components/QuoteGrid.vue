2e067a9eac226deeb88174964fe9ebf371b9806f
Cosmin Panzariuc <cosmin.panzariuc@gmail.com>
2018-01-05 19:28:01 +0200

    fixing stuff

--- quotes_app/src/components/QuoteGrid.vue
+++ quotes_app/src/components/QuoteGrid.vue
@@ -1,19 +1,25 @@
 <template>
-    <div class="row">
-        <app-quote v-for="quote in quotes" :key="quote">
-            {{quote}}
-        </app-quote>
-    </div>
+  <div class="row">
+    <app-quote v-for="quote in quotes" :key="quote">
+      {{quote}}
+    </app-quote>
+  </div>
 </template>
 
 <script>
-    import Quote from './Quote';
-    export default {
-        props: ['quotes'],
-        components:{
-            appQuote: Quote
-        }
+  import Quote from './Quote';
+  export default {
+//    props: ['quotes'],
+    props: {
+      quotes: {
+        type: Array,
+        required: true
+      }
+    },
+    components: {
+      appQuote: Quote
     }
+  }
 </script>
 
 <style scoped>
