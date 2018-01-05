939f392ce0386f48c2dbac9a90a408f7048eb380
Cosmin Panzariuc <cosmin.panzariuc@avangate.com>
2018-01-05 17:22:27 +0200

    Quote app - first commit

---
+++ quotes_app/src/App.vue
@@ -0,0 +1,28 @@
+<template>
+    <div id="container">
+        <app-quote-grid :quotes="quotes"></app-quote-grid>
+    </div>
+</template>
+
+<script type="text/babel">
+    import QuoteGrid from './components/QuoteGrid';
+
+    export default {
+        data(){
+            return{
+                quotes: [
+                    'Just a Quote too see something'
+                ],
+                maxQuotes: 10
+            }
+        },
+
+        components:{
+            appQuoteGrid: QuoteGrid
+        }
+    }
+</script>
+
+<style>
+
+</style>
