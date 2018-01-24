f98f2a095ada5748abbf9b2a5be7184db2729992
Cosmin Panzariuc <cosmin.panzariuc@gmail.com>
2018-01-24 15:55:19 +0200

    add Vuex project

---
+++ VUEX - Max/src/components/AnotherCounter.vue
@@ -0,0 +1,21 @@
+<template>
+    <div>
+        <button class="btn btn-primary" @click="asyncIncrement({by: 100, duration:500})">Increment</button>
+        <button class="btn btn-primary" @click="asyncDecrement({by: 100, duration:500})">Decrement</button>
+    </div>
+</template>
+
+<script type="text/babel">
+    import {mapMutations} from 'vuex';
+    import {mapActions} from 'vuex';
+
+    export default {
+
+        methods: {
+            ...mapActions(['asyncIncrement', 'asyncDecrement'])
+        }
+    }
+
+</script>
+
+
