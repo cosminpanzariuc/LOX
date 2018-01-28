0acbe863375ec5a9ca11fe14a56721a2fc5ca85a
Cosmin Panzariuc <cosmin.panzariuc@gmail.com>
2018-01-28 21:03:25 +0200

    add new Vue project - The Stock Trader

--- VueJS - The Stock Trader/src/main.js
+++ VueJS - The Stock Trader/src/main.js
@@ -1,7 +1,22 @@
+Vue.config.productionTip = false;
 import Vue from 'vue'
 import App from './App.vue'
+import VueRouter from 'vue-router';
+import {routes} from './routes';
+import store from './store/store';
+
+
+Vue.use(VueRouter);
+
+
+var router = new VueRouter({
+    mode: 'history',
+    routes: routes
+});
 
 new Vue({
-  el: '#app',
-  render: h => h(App)
-})
+    el: '#app',
+    router: router,
+    store: store,
+    render: h => h(App)
+});
---
+++ VueJS - The Stock Trader/src/store/modules/portfolio.js
@@ -0,0 +1,59 @@
+const state = {
+    funds: 10000,
+    stocks: []
+};
+
+const mutations = {
+    'BUY_STOCK'(state, order){
+        const record = state.stocks.find(element => element.id == order.stockId);
+        if(record){
+            record.quantity += order.quantity;
+        }else{
+            state.stocks.push({
+                id: order.stockId,
+                quantity: order.quantity
+            });
+        }
+        state.funds -= order.stockPrice * order.quantity;
+    },
+
+    'SELL_STOCK'(state, order){
+        const record = state.stocks.find(element => element.id == order.stockId);
+        if(record.quantity > order.quantity){
+            record.quantity -= order.quantity;
+        }else{
+            state.stocks.splice(state.stocks.indexOf(record), 1);
+        }
+        state.funds += order.stockPrice * order.quantity;
+    }
+};
+
+const actions = {
+    sellStock: (context, order) => {
+        context.commit('SELL_STOCK', order);
+    }
+};
+
+const getters = {
+    stockPortfolio(state, getters){
+        return state.stocks.map(stock => {
+            const record = getters.stocks.find(element => element.id == stock.id);
+            return {
+                id: stock.id,
+                quantity: stock.quantity,
+                name: record.name,
+                price: record.price
+            }
+        });
+    },
+    funds(state){
+        return state.funds;
+    }
+};
+
+export default{
+    state: state,
+    mutations: mutations,
+    actions: actions,
+    getters: getters
+}
