dc79464c4da1265c301b787a75768fb358c4d4a0
Cosmin Panzariuc <cosmin.panzariuc@gmail.com>
2018-05-20 09:06:41 +0300

    small change

--- VueJS - The Stock Trader/src/main.js
+++ VueJS - The Stock Trader/src/main.js
@@ -1,21 +1,19 @@
 Vue.config.productionTip = false;
 import Vue from 'vue'
 import App from './App.vue'
+import store from './store/store';
 
 import VueRouter from 'vue-router';
 Vue.use(VueRouter);
 
-import VueResource from 'vue-resource';
-Vue.use(VueResource);
-
 import {routes} from './routes';
-import store from './store/store';
-
-
 var router = new VueRouter({
     mode: 'history',
     routes: routes
 });
+
+import VueResource from 'vue-resource';
+Vue.use(VueResource);
 
 Vue.http.options.root = 'https://vuejs-stock-trader-345ff.firebaseio.com/';
 
