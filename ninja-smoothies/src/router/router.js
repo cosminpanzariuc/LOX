f1c6371595334e706dd67b026c7a031b46e9f45f
Cosmin Panzariuc <cosmin.panzariuc@gmail.com>
2018-06-03 11:48:36 +0300

    add Ninja-Smoothies project

--- ninja-smoothies/src/router/router.js
+++ ninja-smoothies/src/router/router.js
@@ -2,6 +2,7 @@ import Vue from 'vue';
 import Router from 'vue-router';
 import Index from '@/components/Index';
 import AddSmoothie from '@/components/AddSmoothie';
+import EditSmoothie from '@/components/EditSmoothie';
 
 Vue.use(Router);
 
@@ -9,6 +10,8 @@ export default new Router({
   mode: 'history',
   routes: [
     {path: '/', name: 'Index', component: Index},
-    {path: '/add-smoothie', name: 'AddSmoothie', component: AddSmoothie}
+    {path: '/add-smoothie', name: 'AddSmoothie', component: AddSmoothie},
+    {path: '/edit-smoothie/:smoothie_slug', name: 'EditSmoothie', component: EditSmoothie},
+    {path: '*', redirect: {name: 'Index'}}
   ]
 })
