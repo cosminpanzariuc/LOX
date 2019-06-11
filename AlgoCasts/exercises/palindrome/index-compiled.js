530f8af13cba8e043e9c55c43de31d38ce281417
Cosmin Panzariuc <cosmin.panzariuc@gmail.com>
2019-06-11 23:07:43 +0300

    add palindrome

--- AlgoCasts/exercises/palindrome/index-compiled.js
+++ AlgoCasts/exercises/palindrome/index-compiled.js
@@ -13,12 +13,8 @@ function palindrome(str) {
     return str.split('').every((char, idx) => {
         return char === str[str.length - idx - 1];
     });
-
 }
 
 module.exports = palindrome;
 
-
-
-
-
+//# sourceMappingURL=index-compiled.js.map
