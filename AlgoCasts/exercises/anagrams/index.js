72144c48efd05c07f6974b44b4d479e1a68d8fa0
Cosmin Panzariuc <cosmin.panzariuc@gmail.com>
2019-06-17 11:34:39 +0300

    add capitalize

--- AlgoCasts/exercises/anagrams/index.js
+++ AlgoCasts/exercises/anagrams/index.js
@@ -18,33 +18,33 @@ function anagrams(stringA, stringB) {
     return cleanString(stringA) === cleanString(stringB);
 
 
-//     const aCharMap = buildCharMap(stringA);
-//     const bCharMap = buildCharMap(stringB);
-//
-//     if (Object.keys(aCharMap).length !== Object.keys(bCharMap).length) {
-//         return false;
-//     }
-//
-//     for (let char in aCharMap) {
-//         if (aCharMap[char] !== bCharMap[char]) {
-//             return false;
-//         }
-//     }
-//     return true;
-// }
-//
-//
+    // const aCharMap = buildCharMap(stringA);
+    // const bCharMap = buildCharMap(stringB);
+    //
+    // if (Object.keys(aCharMap).length !== Object.keys(bCharMap).length) {
+    //     return false;
+    // }
+    //
+    // for (let char in aCharMap) {
+    //     if (aCharMap[char] !== bCharMap[char]) {
+    //         return false;
+    //     }
+    // }
+    // return true;
+}
 
 
 
-// function buildCharMap(str) {
-//     var charMap = {};
-//
-//     for (let char of str.replace(/[^\w]/g, '').toLowerCase()) {
-//         charMap[char] = charMap[char]++ || 1;
-//     }
-//
-//     return charMap;
+
+
+function buildCharMap(str) {
+    var charMap = {};
+
+    for (let char of str.replace(/[^\w]/g, '').toLowerCase()) {
+        charMap[char] = charMap[char]++ || 1;
+    }
+
+    return charMap;
 }
 
 module.exports = anagrams;
