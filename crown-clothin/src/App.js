215965de8190446a850f8fe301587cb5d18ae666
Cosmin Panzariuc <cosmin.panzariuc@2checkout.com>
2019-10-24 17:48:32 +0300

    start e-commerce project

--- crown-clothin/src/App.js
+++ crown-clothin/src/App.js
@@ -1,8 +1,7 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+import Homepage from './pages/homepage/homepage.component';
 
-import Homepage from './homepage.component';
 
 function App() {
   return (
