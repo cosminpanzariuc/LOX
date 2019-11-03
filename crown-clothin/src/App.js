b0f6538a24e4dbf9dbc61791a1949c05b2f59956
Cosmin Panzariuc <cosmin.panzariuc@gmail.com>
2019-11-03 16:56:16 +0200

    add files

--- crown-clothin/src/App.js
+++ crown-clothin/src/App.js
@@ -1,14 +1,22 @@
 import React from 'react';
+import {Switch, Route} from 'react-router-dom';
 import './App.css';
-import Homepage from './pages/homepage/homepage.component';
+import HomePage from './pages/homepage/homepage.component';
+import ShopPage from './pages/shop/shop.component';
+import Header from './components/header/header.component'
 
 
 function App() {
   return (
     <div>
-      <Homepage />
+     <Header/>
+     <Switch>
+         <Route exact path='/' component={HomePage}></Route>
+         <Route path='/shop' component={ShopPage}></Route>
+     </Switch>
     </div>
   );
 }
 
 export default App;
+
