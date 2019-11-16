58cea86e102a034099984553af973b3f32f299e8
Cosmin Panzariuc <cosmin.panzariuc@gmail.com>
2019-11-16 23:54:47 +0200

    add files

--- crown-clothin/src/App.js
+++ crown-clothin/src/App.js
@@ -3,7 +3,10 @@ import {Switch, Route} from 'react-router-dom';
 import './App.css';
 import HomePage from './pages/homepage/homepage.component';
 import ShopPage from './pages/shop/shop.component';
-import Header from './components/header/header.component'
+import Header from './components/header/header.component';
+import SignInAndSignUpPage from './pages/sign-in-and-sign-up/sign-in-and-sign-up.component';
+
+
 
 
 function App() {
@@ -13,6 +16,7 @@ function App() {
      <Switch>
          <Route exact path='/' component={HomePage}></Route>
          <Route path='/shop' component={ShopPage}></Route>
+         <Route path='/signin' component={SignInAndSignUpPage}></Route>
      </Switch>
     </div>
   );
--- crown-clothin/src/components/menu-item/menu-item.component.jsx
+++ crown-clothin/src/components/menu-item/menu-item.component.jsx
@@ -10,7 +10,7 @@ const MenuItem = ({ title, imageUrl, size, history, linkUrl, match }) => (
           style={{
             backgroundImage: `url(${imageUrl})`
           }}
-        />
+        ></div>
 
         <div className="content">
             <h1 className="title">{title.toUpperCase()}</h1>
